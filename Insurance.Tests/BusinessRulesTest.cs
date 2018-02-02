5ed5086e94d985289de35fb592662d6e7ae6d325
Luisa Castaño <luilu92@gmail.com>
2018-02-02 11:19:36 -0500

    BusinessRulesTest

---
+++ Insurance.Tests/BusinessRulesTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Insurance.Core.Services;
+
+namespace Insurance.Tests
+{
+    [TestClass]
+    public class BusinessRulesTest
+    {
+        [TestMethod()]
+        public void CheckRiskKindTest()
+        {
+            var risk = 1;
+            var percent = 50;
+            var expect = true;
+            var sevice = new Service();
+            bool actual = sevice.CheckRiskKind(risk, percent);
+            Assert.AreEqual(expect, actual);
+        }
+    }
+}
